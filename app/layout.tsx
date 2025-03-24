b7ae3cb84865d2e0c6284f9962074baa56c92375
Pamela Ocampo <pamela.ocampo@gmail.com>
2025-03-23 17:38:34 -0700

    Add analytics and update next

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
-import type React from 'react';
 import '@/app/globals.css';
+
+import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
+import type React from 'react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -16,6 +19,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
         {children}
         <Toaster />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
